5b16cfb3e73021c81ae16c67f018ed065b9444c3
Dario Miguel <dario.e.miguel@gmail.com>
2026-05-30 12:30:22 -0300

    chore: Elementos iniciales en base a proyecto MVC trabajado en clase.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Texto de prueba";
+//require_once __DIR__ . '/helpers/Autoloader.php';
+//$config = new Configurator();
+//$router = $config->getRouter();
+//
+//$router->dispatch(
+//    $_GET['controller'] ?? '',
+//        $_GET['method'] ?? ''
+//);
