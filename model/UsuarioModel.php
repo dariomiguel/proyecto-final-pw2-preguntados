ece7c043aa08d8faf581bc574e38fa8ba476d5d2
Bruno <brunosmerlo@gmail.com>
2026-06-18 17:09:12 -0300

    feat: implementar edicion de perfil completa y ajuste visual

--- model/UsuarioModel.php
+++ model/UsuarioModel.php
@@ -16,4 +16,15 @@ class UsuarioModel
         $resultado = $this->database->query($sql, [$nombreUsuario]);
         return !empty($resultado) ? $resultado[0] : null;
     }
+
+    public function editarPerfil($nombre, $segundoNombre, $apellido, $anioNacimiento, $sexo, $pais, $ciudad, $mail, $contrasenia, $nuevoNombreUsuario, $nombreUsuarioActual){
+
+        $sql = "UPDATE usuarios 
+            SET nombre = ?, segundo_nombre = ?, apellido = ?, anio_nacimiento = ?, sexo = ?, pais = ?, ciudad = ?, mail = ?, contrasenia = ?, nombre_usuario = ? 
+            WHERE nombre_usuario = ?";
+
+        $parametros = [$nombre, $segundoNombre, $apellido, $anioNacimiento, $sexo, $pais, $ciudad, $mail, $contrasenia, $nuevoNombreUsuario, $nombreUsuarioActual];
+
+        return $this->database->execute($sql, $parametros);
+    }
 }
